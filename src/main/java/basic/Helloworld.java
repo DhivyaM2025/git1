0e80e89433704a2029fd7c28cccf8a6fb05aa719
DhivyaM2025 <dhivya.db.2025@gmail.com>
2025-07-16 10:29:21 +0530

    updated code

--- src/main/java/basic/Helloworld.java
+++ src/main/java/basic/Helloworld.java
@@ -6,7 +6,6 @@ public class Helloworld {
 	    }
 //b1 code changes
 	    public void m1() {
-	    	System.out.println("from m1"); //c1 code changes ++ //d1 code change ++//dev2 changes.. ++ //dev1 changes ++ /y1 code change
-
+	    	System.out.println("from m1"); //c1 code changes ++ //d1 code change ++//dev2 changes.. ++ //dev1 changes ++ /y1 code change ++/x1 code changes
 	    }
 }
